f5296e869ab826562326b96046fd9a647f41ca69
Sam <ke.sk.usta@hotmail.com>
2020-11-28 06:48:02 +0200

    add smoketest shoppinglistcontroller

--- src/test/java/com/example/shoppinglist/ShoppinglistApplicationTests.java
+++ src/test/java/com/example/shoppinglist/ShoppinglistApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.shoppinglist;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.shoppinglist.web.ShoppinglistController;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ShoppinglistApplicationTests {
+@AutoConfigureMockMvc
+public class ShoppinglistApplicationTests {
+
+	@Autowired
+	private ShoppinglistController sController;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(sController).isNotNull();
 	}
 
 }
